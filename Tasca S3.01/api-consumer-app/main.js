088f191f82facc32bba58b9468f20a7dc27fea8f
albert-dev <al96@hotmail.es>
2026-06-15 12:31:19 +0200

    feature: "created const to get all of the DOM values as the exercice demands"

--- Tasca S3.01/api-consumer-app/main.js
+++ Tasca S3.01/api-consumer-app/main.js
@@ -1,1 +1,17 @@
 import axios, { isCancel, AxiosError } from "axios";
+
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+let currentPage = 1;
+const itemsPerPage = 10;
+
+const apiSelector = document.getElementById("api-select").value
+const searchInput = document.getElementById("search-input").value
+const fetchButton = document.getElementById("btn-get-data")
+const loadingElement = document.getElementById("loading-status")
+const errorElement = document.getElementById("error-message")
+const resultsContainer = document.getElementById("results-container")
+const paginationContainer = document.getElementById("pagination")
+
+
+
+
