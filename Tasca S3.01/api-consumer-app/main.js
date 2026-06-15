787f366d52cb29fd3d20cf05a34f91301e5cc967
albert-dev <al96@hotmail.es>
2026-06-15 12:56:04 +0200

    feature: "created function fetchData skeleton that we will use to fetch data using Axios or Fetch, alongside with a function to display the results on the DOM and another tunction to setup the pagination"

--- Tasca S3.01/api-consumer-app/main.js
+++ Tasca S3.01/api-consumer-app/main.js
@@ -4,8 +4,8 @@ const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 let currentPage = 1;
 const itemsPerPage = 10;
 
-const apiSelector = document.getElementById("api-select").value
-const searchInput = document.getElementById("search-input").value
+const apiSelector = document.getElementById("api-select")
+const searchInput = document.getElementById("search-input")
 const fetchButton = document.getElementById("btn-get-data")
 const loadingElement = document.getElementById("loading-status")
 const errorElement = document.getElementById("error-message")
@@ -28,6 +28,40 @@ function showError(message) {
 
 function hideError() {
     errorElement.hidden = false
+}
+
+async function fetchData() {
+    const searchTerm = searchInput.value
+    const useAxios = apiSelector.value === "axios"
+    
+    showLoading();
+    hideError();
+
+    try {
+        if (useAxios) {
+            fetchDataWithAxios()
+        } else {
+            fetchDataWithFetch();
+        }
+    } catch (error) {
+        // ... (Gestiona errors inesperats si s'escapen de les funcions específiques de Fetch/Axios)
+    } finally {
+        hideLoading();
+    }
+
+
+function displayResults(items, totalItems) {
+
+}
+
+function setupPagination(totalItems) {
+
+}
+}
+
+async function fetchDataWithFetch(searchTerm) {
+}
+async function fetchDataWithAxios(searchTerm) {
 }
 
 
