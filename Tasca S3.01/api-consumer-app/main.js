4c2e2e5eb9243e9972a0f077aa40bf48c6283685
albert-dev <al96@hotmail.es>
2026-06-16 12:14:30 +0200

    feature: "finalised displayResults function"

--- Tasca S3.01/api-consumer-app/main.js
+++ Tasca S3.01/api-consumer-app/main.js
@@ -51,17 +51,26 @@ async function fetchData() {
 
 
 function displayResults(items, totalItems) {
-    resultsContainer = " "
+    resultsContainer.textContent = ""
 
     if (items.length === 0){
-        return resultsContainer.textContent = "No s'han trobat results" 
+        return resultsContainer.textContent = "No results were found" 
     }
 
+    items.forEach((item) => {
+    const card = document.createElement("div")
+    card.classList.add("card")
+    card.innerHTML = `
+    <h3> ${item.title} </h3>
+    <p> ${item.body} </p>
+    <p> ${item.id} </p>
+    `
+    resultsContainer.appendChild(card)
+
+    });
     
 
-
-
-
+    setupPagination(totalItems)
 }
 
 function setupPagination(totalItems) {
