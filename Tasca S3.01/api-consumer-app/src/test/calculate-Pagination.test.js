87a74b5e0b0d69b5fd602ad4d37176db3e95eb71
albert-dev <al96@hotmail.es>
2026-06-18 12:45:22 +0200

    feature: "added additional tests"

--- Tasca S3.01/api-consumer-app/src/test/calculate-Pagination.test.js
+++ Tasca S3.01/api-consumer-app/src/test/calculate-Pagination.test.js
@@ -10,6 +10,12 @@ describe('Testing calculatePagination function', () => {
     expect(calculatePagination(50, 10)).toBe(5);
     expect(calculatePagination(10, 10)).toBe(1);
     expect(calculatePagination(100, 10)).toBe(10);
+    expect(calculatePagination(33, 10)).toBe(4);
+    expect(calculatePagination(42, 10)).toBe(5);
+    expect(calculatePagination(0, 10)).toBe(0);
+    expect(calculatePagination(55, 10)).toBe(6);
+    expect(calculatePagination(1, 10)).toBe(1);
+    expect(calculatePagination(72, 10)).toBe(8);
 
     });
 });
